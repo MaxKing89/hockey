8a072223545575edafe98a183efad6fa542feea0
MaxKing89 <iuking16@gmail.com>
2015-10-27 20:26:17 -0700

    utf

--- dev/wp-config.php
+++ dev/wp-config.php
@@ -32,7 +32,7 @@ define('DB_PASSWORD', 'Hockey10!$');
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'UTF-8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
